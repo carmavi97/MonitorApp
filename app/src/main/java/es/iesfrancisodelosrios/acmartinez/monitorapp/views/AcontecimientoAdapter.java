15c9b826dee46ed557438f70887031a472a7cedd
carlos <carmavi97@gmail.com>
2019-11-25 14:38:13 +0100

    commit 7.5

---
+++ app/src/main/java/es/iesfrancisodelosrios/acmartinez/monitorapp/views/AcontecimientoAdapter.java
@@ -0,0 +1,93 @@
+package es.iesfrancisodelosrios.acmartinez.monitorapp.views;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import es.iesfrancisodelosrios.acmartinez.monitorapp.R;
+import es.iesfrancisodelosrios.acmartinez.monitorapp.model.Person;
+
+/**
+ * Clase para almacenar el adaptador con los datos
+ * de los acontecimientos que va a mostrar
+ * el RecyclerView
+ *
+ * Hay que añadir al proyecto la siguiente
+ * dependencia en el archivo /app/build.gradle
+ * con la versión que estemos trabajando
+ * 'com.android.support:recyclerview-v7:27.1.1'
+ */
+public class AcontecimientoAdapter extends RecyclerView.Adapter<AcontecimientoAdapter.AcontecimientoViewHolder>
+        implements View.OnClickListener {
+    private ArrayList<Person> items;
+    private View.OnClickListener listener;
+
+    // Contruye el objeto adaptador recibiendo la lista de datos
+    public AcontecimientoAdapter(@NonNull ArrayList<Person> items) {
+        this.items = items;
+    }
+
+    // Se encarga de crear los nuevos objetos ViewHolder necesarios
+    // para los elementos de la colección.
+    // Infla la vista del layout, crea y devuelve el objeto ViewHolder
+    @Override
+    public AcontecimientoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View row = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row, parent, false);
+        row.setOnClickListener(this);
+
+        AcontecimientoViewHolder avh = new AcontecimientoViewHolder(row);
+        return avh;
+    }
+
+    // Se encarga de actualizar los datos de un ViewHolder ya existente.
+    @Override
+    public void onBindViewHolder(AcontecimientoViewHolder viewHolder, int position) {
+        Person item = items.get(position);
+        viewHolder.AcontecimientoBind(item);
+    }
+
+    // Indica el número de elementos de la colección de datos.
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    // Asigna un listener al elemento
+    public void setOnClickListener(View.OnClickListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(listener != null)
+            listener.onClick(view);
+    }
+
+    // Clase interna:
+    // Se implementa el ViewHolder que se encargará
+    // de almacenar la vista del elemento y sus datos
+    public static class AcontecimientoViewHolder
+            extends RecyclerView.ViewHolder {
+
+        private TextView TextView_id;
+        private TextView TextView_nombre;
+
+        public AcontecimientoViewHolder(View itemView) {
+            super(itemView);
+            TextView_id = (TextView) itemView.findViewById(R.id.id);
+            TextView_nombre = (TextView) itemView.findViewById(R.id.name);
+        }
+
+        public void AcontecimientoBind(Person item) {
+            TextView_id.setText(item.getId());
+            TextView_nombre.setText(item.getFullname());
+        }
+    }
+}
